c4e7fc3f89410a5e304e3faee15b74257baf90a6
Dani <damuru88@gmail.com>
2024-02-08 13:18:46 +0100

    add transitions

--- apps/editor/src/ui/BookPreview/index.tsx
+++ apps/editor/src/ui/BookPreview/index.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react'
+import {FC, useEffect, useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 
 import type {BodyJSON} from '../../domain/body/Models/Body'
@@ -15,58 +15,76 @@ interface Props {
 }
 
 const MOCKED_ROOT_CHAPTER = '01HP17MPVZHZR45DX06NCND4KF'
+const TRANSITION_DURATION = 300
 
 export const BookPreview: FC<Props> = ({book, chapter, body, links}) => {
   const navigate = useNavigate()
+  const [isVisible, setIsVisible] = useState(true)
   const bookId = String(book.id)
 
-  const handleLinkClick = (link: LinkJSON) => {
-    const chapter = link.toChapter
-    const chapterId = String(chapter?.id)
-    const url = `/book/${bookId}/preview/${chapterId}`
-    if (url) navigate(url)
+  const handleLinkClick = (chapterId: String) => {
+    setIsVisible(false)
+    setTimeout(() => {
+      const url = `/book/${bookId}/preview/${String(chapterId)}`
+      navigate(url)
+      setIsVisible(true)
+    }, TRANSITION_DURATION)
   }
 
+  useEffect(() => {
+    setIsVisible(false)
+    const timeoutId = setTimeout(() => setIsVisible(true), TRANSITION_DURATION)
+    return () => clearTimeout(timeoutId)
+  }, [chapter.id])
+
   return (
-    <div className="flex flex-col h-full relative  font-prose">
+    <div className="bg-gray-900 h-full">
       <div
-        className="absolute inset-0 z-0 bg-cover bg-center"
-        style={{backgroundImage: `url(${previewBackground})`}}
-      ></div>
-      <div className="text-center text-white p-4 z-10 bg-gray-900/80">
-        <h1 className="text-3xl font-bold">{book.title}</h1>
-        <h2 className="text-2xl">{chapter.title}</h2>
-      </div>
-      <div className="flex flex-1 overflow-auto relative items-center justify-center	">
-        <div className="absolute top-0 left-0 right-0 h-10 bg-gradient-to-b from-gray-900/80 to-transparent"></div>
-        <div className="absolute bottom-0 left-0 right-0 h-10 bg-gradient-to-t from-gray-800/80 to-transparent"></div>
+        className={`flex flex-col min-h-full relative font-prose transition-opacity ease-in duration-${TRANSITION_DURATION} ${
+          isVisible ? 'opacity-100' : 'opacity-0'
+        }`}
+      >
         <div
-          className="z-10 relative bg-slate-100/40 p-4 m-4 h-fit rounded-lg text-center text-pretty backdrop-blur-sm text-2xl"
-          dangerouslySetInnerHTML={{__html: body.content.slice(1, -1)}}
-        />
-      </div>
-      <div className="bg-gray-800/80 z-10">
-        <ul className="flex flex-col justify-around items-center p-2 cursor-pointer">
-          {links.map(link => {
-            return (
+          className="absolute inset-0 z-0 bg-cover bg-center"
+          style={{backgroundImage: `url(${previewBackground})`}}
+        ></div>
+        <div className="text-center text-white p-4 z-10 bg-gray-900/80">
+          <h1 className="text-3xl font-bold">{book.title}</h1>
+          <h2 className="text-2xl">{chapter.title}</h2>
+        </div>
+        <div className="flex flex-1 overflow-auto relative items-center justify-center	">
+          <div className="absolute top-0 left-0 right-0 h-10 bg-gradient-to-b from-gray-900/80 to-transparent"></div>
+          <div className="absolute bottom-0 left-0 right-0 h-10 bg-gradient-to-t from-gray-800/80 to-transparent"></div>
+          <div
+            className="z-10 relative bg-slate-100/40 p-4 my-6 mx-2 h-fit rounded-lg text-center text-pretty backdrop-blur-sm text-2xl"
+            dangerouslySetInnerHTML={{__html: body.content.slice(1, -1)}}
+          />
+        </div>
+        <div className="bg-gray-800/80 z-10">
+          <ul className="flex flex-col justify-around items-center p-2 cursor-pointer">
+            {links.map(link => {
+              const linkChapter = link.toChapter
+              const linkChapterId = String(linkChapter?.id)
+              return (
+                <li
+                  key={link.id}
+                  className="m-2 pointer bg-slate-100/10 text-slate-100/80  px-4 py-2 rounded-lg hover:bg-slate-100/20 hover:text-slate-100 transition-all duration-200 ease-in-out w-full text-center text-2xl backdrop-blur-sm"
+                  onClick={() => handleLinkClick(linkChapterId)}
+                >
+                  {link.body}
+                </li>
+              )
+            })}
+            {!links.length && (
               <li
-                key={link.id}
-                className="m-2 pointer bg-slate-100/10 text-slate-100/80  px-4 py-2 rounded-lg hover:bg-slate-100/20 hover:text-slate-100 transition-all duration-200 ease-in-out w-full text-center text-2xl"
-                onClick={() => handleLinkClick(link)}
+                className="m-2 pointer bg-slate-100/10 text-slate-100/80  px-4 py-2 rounded-lg hover:bg-slate-100/20 hover:text-slate-100 transition-all duration-200 ease-in-out w-full text-center text-2xl backdrop-blur-sm"
+                onClick={() => handleLinkClick(MOCKED_ROOT_CHAPTER)}
               >
-                {link.body}
+                Volver a emperzar
               </li>
-            )
-          })}
-          {!links.length && (
-            <li
-              className="m-2 pointer bg-slate-100/10 text-slate-100/80  px-4 py-2 rounded-lg hover:bg-slate-100/20 hover:text-slate-100 transition-all duration-200 ease-in-out w-full text-center text-2xl"
-              onClick={() => navigate(`/book/${bookId}/preview/${MOCKED_ROOT_CHAPTER}`)}
-            >
-              Volver a emperzar
-            </li>
-          )}
-        </ul>
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   )
--- apps/editor/src/ui/IphoneMockup/index.tsx
+++ apps/editor/src/ui/IphoneMockup/index.tsx
@@ -11,7 +11,11 @@ export const IphoneMockup: FC<Props> = ({children}) => {
       <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
       <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
       <div className="h-[64px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
-      <div className="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white">{children}</div>
+      <div className="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white">
+        <div className="h-full overflow-auto bg-gray-900 hide-scrollbar" style={{WebkitOverflowScrolling: 'touch'}}>
+          {children}
+        </div>
+      </div>
     </div>
   )
 }
