f00ab810a1bd78dd763df73c25f86c158ee5aa17
Dani <damuru88@gmail.com>
2024-02-12 15:00:19 +0100

    fix prettier

--- apps/editor/src/domain/index.ts
+++ apps/editor/src/domain/index.ts
@@ -63,38 +63,38 @@ export class Domain {
   }
 
   /** AUTH */
-  get LogoutAuthUseCase() {return this.#getter<void, AuthTokens>(async () => import('./auth/UseCases/LogoutAuthUseCase.js'), 'LogoutAuthUseCase')} // eslint-disable-line 
-  get LoginAuthUseCase() {return this.#getter<LoginAuthUseCaseInput, AuthTokens>(async () => import('./auth/UseCases/LoginAuthUseCase.js'), 'LoginAuthUseCase')} // eslint-disable-line 
+  get LogoutAuthUseCase() {return this.#getter<void, AuthTokens>(async () => import('./auth/UseCases/LogoutAuthUseCase.js'), 'LogoutAuthUseCase')} // eslint-disable-line
+  get LoginAuthUseCase() {return this.#getter<LoginAuthUseCaseInput, AuthTokens>(async () => import('./auth/UseCases/LoginAuthUseCase.js'), 'LoginAuthUseCase')} // eslint-disable-line
 
   /** USER */
-  get CreateUserUseCase() {return this.#getter<CreateUserUseCaseInput, User>(async () => import('./user/UseCases/CreateUserUseCase.js'), 'CreateUserUseCase')} // eslint-disable-line 
-  get CurrentUserUseCase() {return this.#getter<void, User>(async () => import('./user/UseCases/CurrentUserUseCase.js'), 'CurrentUserUseCase')} // eslint-disable-line 
+  get CreateUserUseCase() {return this.#getter<CreateUserUseCaseInput, User>(async () => import('./user/UseCases/CreateUserUseCase.js'), 'CreateUserUseCase')} // eslint-disable-line
+  get CurrentUserUseCase() {return this.#getter<void, User>(async () => import('./user/UseCases/CurrentUserUseCase.js'), 'CurrentUserUseCase')} // eslint-disable-line
 
   /** Book */
-  get FindByIDBookUseCase() {return this.#getter<FindByIDBookUseCaseInput, Book>(async () => import('./book/UseCases/FindByIDBookUseCase.js'), 'FindByIDBookUseCase')} // eslint-disable-line 
-  get CreateBookUseCase() {return this.#getter<CreateBookUseCaseInput, Book>(async () => import('./book/UseCases/CreateBookUseCase.js'), 'CreateBookUseCase')} // eslint-disable-line 
-  get UpdateBookUseCase() {return this.#getter<UpdateBookUseCaseInput, Book>(async () => import('./book/UseCases/UpdateBookUseCase.js'), 'UpdateBookUseCase')} // eslint-disable-line 
-  get GetAllBookUseCase() {return this.#getter<void, Books>(async () => import('./book/UseCases/GetAllBookUseCase.js'), 'GetAllBookUseCase')} // eslint-disable-line 
+  get FindByIDBookUseCase() {return this.#getter<FindByIDBookUseCaseInput, Book>(async () => import('./book/UseCases/FindByIDBookUseCase.js'), 'FindByIDBookUseCase')} // eslint-disable-line
+  get CreateBookUseCase() {return this.#getter<CreateBookUseCaseInput, Book>(async () => import('./book/UseCases/CreateBookUseCase.js'), 'CreateBookUseCase')} // eslint-disable-line
+  get UpdateBookUseCase() {return this.#getter<UpdateBookUseCaseInput, Book>(async () => import('./book/UseCases/UpdateBookUseCase.js'), 'UpdateBookUseCase')} // eslint-disable-line
+  get GetAllBookUseCase() {return this.#getter<void, Books>(async () => import('./book/UseCases/GetAllBookUseCase.js'), 'GetAllBookUseCase')} // eslint-disable-line
 
   /** Chapter */
-  get CreateChapterUseCase() {return this.#getter<CreateChapterUseCaseInput, Chapter>(async () => import('./chapter/UseCases/CreateChapterUseCase.js'), 'CreateChapterUseCase')} // eslint-disable-line 
-  get RemoveByIDChapterUseCase() {return this.#getter<RemoveByIDChapterUseCaseInput, Chapter>(async () => import('./chapter/UseCases/RemoveByIDChapterUseCase.js'), 'RemoveByIDChapterUseCase')} // eslint-disable-line 
-  get UpdateChapterUseCase() {return this.#getter<UpdateChapterUseCaseInput, Chapter>(async () => import('./chapter/UseCases/UpdateChapterUseCase.js'), 'UpdateChapterUseCase')} // eslint-disable-line 
-  get FindByIDChapterUseCase() {return this.#getter<FindByIDChapterUseCaseInput, Chapter>(async () => import('./chapter/UseCases/FindByIDChapterUseCase.js'), 'FindByIDChapterUseCase')} // eslint-disable-line 
-  get GetAllChapterUseCase() {return this.#getter<GetAllChapterUseCaseInput, Chapters>(async () => import('./chapter/UseCases/GetAllChapterUseCase.js'), 'GetAllChapterUseCase')} // eslint-disable-line 
-  get GetRootChapterUseCase() {return this.#getter<GetRootChapterUseCaseInput, Chapter>(async () => import('./chapter/UseCases/GetRootChapterUseCase.js'),'GetRootChapterUseCase')} // eslint-disable-line
+  get CreateChapterUseCase() {return this.#getter<CreateChapterUseCaseInput, Chapter>(async () => import('./chapter/UseCases/CreateChapterUseCase.js'), 'CreateChapterUseCase')} // eslint-disable-line
+  get RemoveByIDChapterUseCase() {return this.#getter<RemoveByIDChapterUseCaseInput, Chapter>(async () => import('./chapter/UseCases/RemoveByIDChapterUseCase.js'), 'RemoveByIDChapterUseCase')} // eslint-disable-line
+  get UpdateChapterUseCase() {return this.#getter<UpdateChapterUseCaseInput, Chapter>(async () => import('./chapter/UseCases/UpdateChapterUseCase.js'), 'UpdateChapterUseCase')} // eslint-disable-line
+  get FindByIDChapterUseCase() {return this.#getter<FindByIDChapterUseCaseInput, Chapter>(async () => import('./chapter/UseCases/FindByIDChapterUseCase.js'), 'FindByIDChapterUseCase')} // eslint-disable-line
+  get GetAllChapterUseCase() {return this.#getter<GetAllChapterUseCaseInput, Chapters>(async () => import('./chapter/UseCases/GetAllChapterUseCase.js'), 'GetAllChapterUseCase')} // eslint-disable-line
+  get GetRootChapterUseCase() {return this.#getter<GetRootChapterUseCaseInput, Chapter>(async () => import('./chapter/UseCases/GetRootChapterUseCase.js'), 'GetRootChapterUseCase')} // eslint-disable-line
 
   /** Link */
-  get GetAllLinkUseCase() {return this.#getter<GetAllLinkUseCaseInput, Links>(async () => import('./link/UseCases/GetAllLinkUseCase.js'), 'GetAllLinkUseCase')} // eslint-disable-line 
-  get FindByIDLinkUseCase() {return this.#getter<FindByIDLinkUseCaseInput, Link>(async () => import('./link/UseCases/FindByIDLinkUseCase.js'), 'FindByIDLinkUseCase')} // eslint-disable-line 
-  get CreateLinkUseCase() {return this.#getter<CreateLinkUseCaseInput, Link>(async () => import('./link/UseCases/CreateLinkUseCase.js'), 'CreateLinkUseCase')} // eslint-disable-line 
-  get RemoveByIDLinkUseCase() {return this.#getter<RemoveByIDLinkUseCaseInput, Link>(async () => import('./link/UseCases/RemoveByIDLinkUseCase.js'), 'RemoveByIDLinkUseCase')} // eslint-disable-line 
+  get GetAllLinkUseCase() {return this.#getter<GetAllLinkUseCaseInput, Links>(async () => import('./link/UseCases/GetAllLinkUseCase.js'), 'GetAllLinkUseCase')} // eslint-disable-line
+  get FindByIDLinkUseCase() {return this.#getter<FindByIDLinkUseCaseInput, Link>(async () => import('./link/UseCases/FindByIDLinkUseCase.js'), 'FindByIDLinkUseCase')} // eslint-disable-line
+  get CreateLinkUseCase() {return this.#getter<CreateLinkUseCaseInput, Link>(async () => import('./link/UseCases/CreateLinkUseCase.js'), 'CreateLinkUseCase')} // eslint-disable-line
+  get RemoveByIDLinkUseCase() {return this.#getter<RemoveByIDLinkUseCaseInput, Link>(async () => import('./link/UseCases/RemoveByIDLinkUseCase.js'), 'RemoveByIDLinkUseCase')} // eslint-disable-line
 
   /** Body */
-  get CommitBodyUseCase() {return this.#getter<CommitBodyUseCaseInput, Body>(async () => import('./body/UseCases/CommitBodyUseCase.js'), 'CommitBodyUseCase')} // eslint-disable-line 
-  get GetLastCommitBodyUseCase() {return this.#getter<GetLastCommitBodyUseCaseInput, Body>(async () => import('./body/UseCases/GetLastCommitBodyUseCase.js'), 'GetLastCommitBodyUseCase')} // eslint-disable-line 
-  get GetLastBodyUseCase() {return this.#getter<GetLastBodyUseCaseInput, Body>(async () => import('./body/UseCases/GetLastBodyUseCase.js'), 'GetLastBodyUseCase')} // eslint-disable-line 
-  get SaveBodyUseCase() {return this.#getter<SaveBodyUseCaseInput, Body>(async () => import('./body/UseCases/SaveBodyUseCase.js'), 'SaveBodyUseCase')} // eslint-disable-line 
+  get CommitBodyUseCase() {return this.#getter<CommitBodyUseCaseInput, Body>(async () => import('./body/UseCases/CommitBodyUseCase.js'), 'CommitBodyUseCase')} // eslint-disable-line
+  get GetLastCommitBodyUseCase() {return this.#getter<GetLastCommitBodyUseCaseInput, Body>(async () => import('./body/UseCases/GetLastCommitBodyUseCase.js'), 'GetLastCommitBodyUseCase')} // eslint-disable-line
+  get GetLastBodyUseCase() {return this.#getter<GetLastBodyUseCaseInput, Body>(async () => import('./body/UseCases/GetLastBodyUseCase.js'), 'GetLastBodyUseCase')} // eslint-disable-line
+  get SaveBodyUseCase() {return this.#getter<SaveBodyUseCaseInput, Body>(async () => import('./body/UseCases/SaveBodyUseCase.js'), 'SaveBodyUseCase')} // eslint-disable-line
 
   #getter<I, O>(loader: Function, name: string) {
     return {
